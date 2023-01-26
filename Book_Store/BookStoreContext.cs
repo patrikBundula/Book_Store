9b333d0992a79dc0587f1528739cb60afb6f78c9
Patrik Bundula <patrik.bundula@gammaorg.hu>
2023-01-26 14:22:56 +0100

    Add project files.

---
+++ Book_Store/BookStoreContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace Book_Store
+{
+
+    public class BookStoreContext : DbContext
+    {
+        //public DbSet<Books> Books { get; set; }
+
+        public BookStoreContext()
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(connectionString: "server=localhost;database=book_store;user=root;password=qpu0DF00", ServerVersion.AutoDetect("server=localhost;database=book_store;user=root;password=qpu0DF00"));
+
+        }
+
+    }
+
+}
---
+++ Book_Store/Books.cs
@@ -0,0 +1,10 @@
+﻿namespace Book_Store
+{
+    //public class Books
+    //{
+    //    public int Id { get; set; }
+    //    public string Title { get; set; }
+
+    //    public float MyProperty { get; set; }
+    //}
+}
