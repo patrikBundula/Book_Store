10fd181e17bbfb674c52b9f53d3ea6fa8450d0a3
Patrik Bundula <patrik.bundula@gammaorg.hu>
2023-02-03 14:30:53 +0100

    add interface to books, and authors

---
+++ Book_Store/Interface/IAuthorRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace Book_Store.Interface
+{
+    public interface IAuthorRepository : IRepository<Author>
+    {
+        Author GetAuthorWithBooks(int id);
+    }
+}
---
+++ Book_Store/Interface/IBookRepository.cs
@@ -0,0 +1,13 @@
+﻿using Book_Store.Dtos;
+namespace Book_Store.Interface
+{
+    public interface IBookRepository : IRepository<Books>
+    {
+        Author GetAuthorById(int authorId);
+
+        Category GetCategoryById(int categoryId);
+
+        Books EditBook(BookDto book);
+        void DeleteBookById(int bookId);
+    }
+}
