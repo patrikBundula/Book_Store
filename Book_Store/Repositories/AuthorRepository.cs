836c99763acd90845a93e5edccb0bbd39445cd82
Patrik Bundula <patrik.bundula@gammaorg.hu>
2023-02-03 14:28:57 +0100

    add author repository

---
+++ Book_Store/Repositories/AuthorRepository.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Book_Store.Interface;
+using Microsoft.EntityFrameworkCore;
+
+namespace Book_Store.Repositories
+{
+    public class AuthorRepository : Repository<Author>, IAuthorRepository
+    {
+
+        public AuthorRepository(BookStoreContext contex, IMapper mapper) : base(contex, mapper)
+        {
+
+        }
+
+        public Author GetAuthorWithBooks(int id)
+        {
+            return BookStoreContext.Authors.Include(a => a.Books).SingleOrDefault(a => a.Id == id);
+        }
+
+        public BookStoreContext BookStoreContext
+        {
+            get { return _dbContext as BookStoreContext; }
+        }
+    }
+}
