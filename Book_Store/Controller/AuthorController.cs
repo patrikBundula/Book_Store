da9a246f498f5ed85334285c9f40c1713a0e994c
Patrik Bundula <patrik.bundula@gammaorg.hu>
2023-02-06 11:52:57 +0100

    add AuthorController

---
+++ Book_Store/Controller/AuthorController.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using Book_Store.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Book_Store.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+        private IUnitOfWork _unitOfWork;
+        private IMapper _mapper;
+
+        public AuthorController(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        public ActionResult<Author> GetAll()
+        {
+
+            var authors = _unitOfWork.AuthorRepository.GetAll();
+
+            if (authors == null) return BadRequest("No Authors in the Db");
+
+            return Ok(authors);
+        }
+        [HttpGet("{id}")]
+        public ActionResult<Author> GetAuthorWithBooks(int id)
+        {
+            var author = _unitOfWork.AuthorRepository.GetAuthorWithBooks(id);
+
+            if (author == null)
+            {
+                return BadRequest(string.Format("Author with id = {0} not found", id)
+);
+            }
+
+            return Ok(author);
+        }
+
+    }
+}
