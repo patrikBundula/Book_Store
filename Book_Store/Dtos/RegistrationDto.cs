018049c4746eff26a2e7d092d4b4fd2495714de4
Patrik Bundula <patrik.bundula@gammaorg.hu>
2023-02-08 16:12:56 +0100

    add user dtos

---
+++ Book_Store/Dtos/RegistrationDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Book_Store.Dtos
+{
+    public class RegistrationDto
+    {
+        public string FirstName { get; init; }
+        public string LastName { get; set; }
+        public string Username { get; init; }
+
+        public string Email { get; init; }
+
+        public string Password { get; set; }
+
+        public string Role { get; init; }
+    }
+}
---
+++ Book_Store/Dtos/UserDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Book_Store.Dtos
+{
+    public class UserDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Email { get; set; }
+
+
+        public string Password { get; set; }
+
+        public float Money = 0;
+
+
+
+
+    }
+}
