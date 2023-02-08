63dfad2f1eeec6384341ca0e1291b854a2faef98
Patrik Bundula <patrik.bundula@gammaorg.hu>
2023-02-08 16:08:50 +0100

    add User Table

---
+++ Book_Store/User.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Book_Store
+{
+    public class User : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public float Money { get; set; }
+
+    }
+
+
+}
