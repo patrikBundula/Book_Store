70c8afe8241ab9b24884c64ab983e7fe77ec531e
Patrik Bundula <patrik.bundula@gammaorg.hu>
2023-02-15 16:16:53 +0100

    add user repo

--- Book_Store/Repositories/UnitOfWork.cs
+++ Book_Store/Repositories/UnitOfWork.cs
@@ -9,13 +9,17 @@ namespace Book_Store.Repositories
     {
         private readonly BookStoreContext _dbContext;
         private readonly IMapper _mapper;
+        private readonly ILoggerFactory _loggerFactory;
+
+
+
 
         public UnitOfWork(BookStoreContext dbContext, IMapper mapper)
         {
             _dbContext = dbContext;
             _mapper = mapper;
         }
-
+        public IUserRepository<string, User> UserRepository => new UserRepository(_mapper, _loggerFactory.CreateLogger<UserRepository>(), _dbContext);
         public IAuthorRepository AuthorRepository => new AuthorRepository(_dbContext, _mapper);
         public IBookRepository BookRepository => new BookRepository(_dbContext, _mapper);
 
