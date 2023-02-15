4131497fd4c948c1e9d9cf31425f63e483bc30c4
Patrik Bundula <patrik.bundula@gammaorg.hu>
2023-02-15 16:12:03 +0100

    add base controller

---
+++ Book_Store/Controller/BaseController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Book_Store.Controller
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+    }
+
+
+}
