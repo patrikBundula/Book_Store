fb455c6345c9880e6b7b4f10e8749935ad4c5c2b
Patrik Bundula <patrik.bundula@gammaorg.hu>
2023-02-24 13:35:06 +0100

    created appConfig class for files

---
+++ Model/AppConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class AppConfig
+    {
+        public string TemplateGeneratedFilesPath { get; set; }
+        public int MaxAllowedFileSizeInMB { get; set; }
+        public int MaxAllowedFileSizeInBytes => MaxAllowedFileSizeInMB * 1048576;
+
+        public string FileStorePath { get; set; }
+    }
+}
